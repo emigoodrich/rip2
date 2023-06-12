73b60ad3d221602e0e67ec5351f8e31b98f930d7
emigoodrich <130619143+emigoodrich@users.noreply.github.com>
2023-06-12 10:41:15 -0600

    inital commit

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,7 @@
+
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+console.log('hello');
+$('li:even').css('background-color', '#ccc');
+$('li:odd').css('background-color','#aaa');
+$('input').val('EMily');
